eac6a65a5805ef94cc11ba3bd42c2c3f89e4b5f6
Михаил Фейгенберг <mrpack801@gmail.com>
2024-02-14 15:33:31 +0300

    14.02.2024

--- prj.lab/stackarr/stackarr.hpp
+++ prj.lab/stackarr/stackarr.hpp
@@ -19,12 +19,30 @@ public:
 	{
 		data_ = new Complex[capacity_];
 	}
-	StackArr(const StackArr&) = default;
+	StackArr(const StackArr& rhs) 
+		: size_{rhs.size_}, capacity_{rhs.capacity_}, data_{new Complex[rhs.capacity_]}
+	{
+		std::copy(rhs.data_ , rhs.data_ + rhs.size_, data_);
+	}
 	~StackArr() {
 		delete[] data_;
 		data_ = nullptr;
 	}
-	StackArr& operator=(const StackArr&) = default;
+	StackArr& operator=(const StackArr& rhs) {
+		if (this != &rhs) {
+			if (rhs.size_ < capacity_) {
+				std::copy(rhs.data_, rhs.data_ + rhs.size_, data_);
+				size_ = rhs.size_;
+				return *this;
+			}
+			delete[] data_;
+			Complex* data_ = new Complex[rhs.size_ * 2];
+			std::copy(rhs.data_, rhs.data_ + rhs.size_, data_);
+			size_ = rhs.size_;
+			capacity_ = rhs.size_ * 2;
+		}
+		return *this;
+	}
 
 	void Pop() noexcept;
 	void Push(const Complex&) noexcept;
--- prj.test/test_stackarr.cpp
+++ prj.test/test_stackarr.cpp
@@ -3,11 +3,23 @@
 int main() {
 	try {
 		StackArr P1;
-		Complex P2(5.0 , 4.0);
-		P1.Push(P2);
-		std::cout << P1.Top() << ' ' << std::endl;
-		P1.Pop();
-		std::cout << P1.IsEmpty();
+		for (int i{0} ; i < 6 ; i++) {
+			P1.Push(Complex(i));
+		}
+		StackArr P2(P1);
+		while (!P2.IsEmpty()) {
+			std::cout << P2.Top() << std::endl;
+			P2.Pop();
+		}
+		P2 = P1;
+		while (!P2.IsEmpty()) {
+			std::cout << P2.Top() << std::endl;
+			P2.Pop();
+		}
+		while(!P1.IsEmpty()) {
+			std::cout << P1.Top() << std::endl;
+			P1.Pop();
+		}
 	}
 	catch (...) {
 		std::cout << "\n bug was detected";
