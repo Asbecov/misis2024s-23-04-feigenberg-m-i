966cdf1ab5d38201ea0e039d565a67e169ff4a22
Asbecov <m2311505@edu.misis.ru>
2024-02-26 15:39:13 +0300

    26.02.2024

--- prj.test/test_stacklst.cpp
+++ prj.test/test_stacklst.cpp
@@ -2,13 +2,13 @@
 
 int main() {
 	try {
-		StackLST P1;
+		StackLst P1;
 		for (int i{1} ; i < 6 ; i++) {
 			P1.Push(Complex(i));
 			std::cout << P1.Top() << ' ';
 		}
 		std::cout << std::endl;
-		StackLST P2(P1);
+		StackLst P2(P1);
 		while (!P2.IsEmpty()) {
 			std::cout << P2.Top() << ' ';
 			P2.Pop();
@@ -22,7 +22,7 @@ int main() {
 			P2.Pop();
 		}
 		std::cout << std::endl;
-		StackLST P3;
+		StackLst P3;
 		P2 = P3;
 		while (!P2.IsEmpty()) {
 			std::cout << P2.Top() << ' ';
