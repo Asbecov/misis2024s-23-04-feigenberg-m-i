bab4aec7f8d2a339fb15fbce0b43848af317fd26
Asbecov <m2311505@edu.misis.ru>
2024-03-02 16:58:11 +0300

    02.03.2024

--- prj.cw/pet_project.cpp
+++ prj.cw/pet_project.cpp
@@ -1,5 +1,38 @@
 #include <opencv2/opencv.hpp>
+#include <cstddef>
+#include <array>
+#include <iostream>
 
-int main() {
-    
+int main(int argcount, char* args[]) {
+    cv::Mat img = cv::imread(args[1], cv::IMREAD_COLOR);
+
+    if (img.empty()) {
+        std::cout << "Could not read the image\n";
+        return 1;
+    }
+
+    std::array<uchar, 256> table;
+    for (int i{0}; i < table.size(); i++) {
+        table[i] = (uchar)(10 * (i / 10));
+    }
+
+    std::ptrdiff_t nrow{img.rows};
+    std::ptrdiff_t ncol{img.cols * img.channels()};
+
+    if (img.isContinuous()) {
+        ncol *= nrow;
+        nrow = 1;
+    }
+
+    uchar* p;
+    for (std::ptrdiff_t i{0}; i < nrow; ++i) {
+        p = img.ptr<uchar>(i);
+        for (std::ptrdiff_t j{0}; j < ncol; ++j) {
+            (p[j]) = table[p[j]]; 
+        }
+    }
+
+    cv::imshow("Display Window", img);
+    cv::waitKey(0);
+    return 0;
 }
