6137114c9d5896145affff42fc4dcb407e002f22
Asbecov <m2311505@edu.misis.ru>
2024-03-16 14:47:41 +0300

    fix: QueueLstPr lib

--- prj.lab/queuelstpr/queuelstpr.cpp
+++ prj.lab/queuelstpr/queuelstpr.cpp
@@ -3,12 +3,20 @@
 void QueueLstPr::Push(const Complex& rhs) noexcept {
     if (last_ != nullptr) {
         Node* head_ = first_;
-        while((head_->next_ != nullptr ? head_->next_->data_ < rhs: true)) {
+        if (head_->data_ > rhs) {
+            first_ = new Node(rhs);
+            first_->next_ = head_;
+            return;
+        }
+        while((head_->next_ != nullptr ? head_->next_->data_ < rhs: false)) {
             head_ = head_->next_;
         }
         Node* temp = head_->next_;
         head_->next_ = new Node(rhs);
         head_->next_->next_ = temp;
+        if (temp == nullptr) {
+            last_ = head_->next_;
+        }
         return;
     }
     first_ = new Node(rhs);
--- prj.lab/queuelstpr/queuelstpr.hpp
+++ prj.lab/queuelstpr/queuelstpr.hpp
@@ -15,6 +15,22 @@ public:
     {
     }
     ~QueueLstPr() = default;
+    QueueLstPr& operator=(const QueueLstPr& rhs) {
+        if (this != &rhs) {
+            while (first_ != nullptr) {
+                Node* temp = first_;
+                first_ = temp->next_;
+                delete temp;
+            }
+            last_ = nullptr;
+            Node* rhs_first = rhs.first_;
+            while (rhs_first != nullptr) {
+                this->Push(rhs_first->data_);
+                rhs_first = rhs_first->next_;
+            }
+        }
+        return *this;
+    }
 
     void Push(const Complex&) noexcept override;
 };
