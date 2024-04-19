b8fa59fdeb10ec76b1412edfb191ae9be5e7e8b6
Asbecov <m2311505@edu.misis.ru>
2024-04-19 16:46:39 +0300

    refactored: stacklst lib test Added Doctest functionality

--- prj.test/test_stacklst.cpp
+++ prj.test/test_stacklst.cpp
@@ -1,42 +1,36 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <stacklst/stacklst.hpp>
+#include "doctest.h"
 
-int main() {
-	try {
-		StackLst P1;
-		for (int i{1} ; i < 6 ; i++) {
-			P1.Push(Complex(i));
-			std::cout << P1.Top() << ' ';
-		}
-		std::cout << std::endl;
-		StackLst P2(P1);
-		while (!P2.IsEmpty()) {
-			std::cout << P2.Top() << ' ';
-			P2.Pop();
-		}
-		std::cout << std::endl;
-		P2 = P1;
-		P1.Pop();
-		P2 = P1;
-		while (!P2.IsEmpty()) {
-			std::cout << P2.Top() << ' ';
-			P2.Pop();
-		}
-		std::cout << std::endl;
-		StackLst P3;
-		P2 = P3;
-		while (!P2.IsEmpty()) {
-			std::cout << P2.Top() << ' ';
-			P2.Pop();
-		}
-		std::cout << std::endl;
-		while(!P1.IsEmpty()) {
-			std::cout << P1.Top() << ' ';
-			P1.Pop();
-		}
-		std::cout << std::endl;
+TEST_CASE("Test constructors") {
+	StackLst P1;
+	CHECK(P1.IsEmpty());
+	StackLst P2(P1);
+	CHECK(P2.IsEmpty());
+	StackLst P3;
+	P3 = P1;
+	CHECK(P3.IsEmpty());
+}
+
+TEST_CASE("Test LIFO stack with StackLst") {
+	StackLst P1;
+	for (int i{1} ; i < 6 ; i++) {
+		P1.Push(Complex(i));
 	}
-	catch (...) {
-		std::cout << "\n bug was detected";
+	StackLst P2(P1);
+	for(int i{5} ; i > 0 ; i--) {
+		CHECK(P2.Top() == Complex(i));
+		P2.Pop();
 	}
+	P2 = P1;
+	P1.Pop();
+	P2 = P1;
+	for(int i{4} ; i > 0 ; i--) {
+		CHECK(P2.Top() == Complex(i));
+		P2.Pop();
+	}
+	StackLst P3;
+	P2 = P3;
+	CHECK(P2.IsEmpty());
 }
  
